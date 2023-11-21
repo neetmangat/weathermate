415cf73a5a6e8d4b07c0cfd4287cdb9ad7e63b29
neetmangat <neetmangat@gmail.com>
2023-11-20 19:09:45 -0800

    tremor rollback to v2 due to bug

--- app/location/[city]/[lat]/[long]/page.tsx
+++ app/location/[city]/[lat]/[long]/page.tsx
@@ -1,4 +1,6 @@
 import { getClient } from "@/apollo-client";
+import CalloutCard from "@/components/CalloutCard";
+import StatCard from "@/components/StatCard";
 import fetchWeatherQuery from "@/graphql/queries/fetchWeatherQueries";
 
 type Props = {
@@ -24,13 +26,44 @@ async function WeatherDashboard({ params: { city, lat, long } }: Props) {
 
   const results: Root = data.myQuery;
 
-  console.log(results);
+  // console.log(results);
 
   return (
     <div>
-      <h1>
-        WeatherDashboard for {city} {lat} {long}
-      </h1>
+      {/* <InformationPanel /> */}
+      <div>
+        <div className="p-5">
+          <div className="pb-5">
+            <h2 className="text-xl font-bold">Today's Overview</h2>
+            <p className="text-sm text-gray-400">
+              Last Updated at:{" "}
+              {new Date(results.current_weather.time).toLocaleString()} (
+              {results.timezone})
+            </p>
+          </div>
+
+          <div className="">
+            <CalloutCard
+              warning
+              message="This is where GPT-4 Summary will go!"
+            />
+          </div>
+
+          <div className="">
+            <StatCard
+              title="Maximum Temperature"
+              metric={`${results.daily.temperature_2m_max[0].toFixed(1)}°C`}
+              color="yellow"
+            />
+
+            <StatCard
+              title="Minimum Temperature"
+              metric={`${results.daily.temperature_2m_min[0].toFixed(1)}°C`}
+              color="green"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
